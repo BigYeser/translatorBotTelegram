02ebe135d8eb01522283e1763a54321269169413
BigYeser <34035117+BigYeser@users.noreply.github.com>
2023-01-20 15:38:31 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 import telegram
 
-API_KEY = '5455052791:AAEpeL_C-BeZD9vatkHdcg3njuOj-qI_C-c'      #ahmad
-#API_KEY = '5052094147:AAHiXRC-4TAR6i2GChGsDlTIUZO3bRMO1yQ'     #testtest
+#API_KEY = '5455052791:AAEpeL_C-BeZD9vatkHdcg3njuOj-qI_C-c'      #ahmad
+API_KEY = '5052094147:AAHiXRC-4TAR6i2GChGsDlTIUZO3bRMO1yQ'     #testtest
 
 KB_OPTION = telegram.ReplyKeyboardMarkup([[telegram.KeyboardButton('الترجمة إلى العربية')],
         [telegram.KeyboardButton('الترجمة إلى الإنجليزية')]])
